402035216a110ea6975093687b4beab734cb9fb6
tjumyk <tjumyk@gmail.com>
2019-07-08 04:24:57 +1000

    add config service

---
+++ services/config.py
@@ -0,0 +1,62 @@
+import os
+from typing import List
+
+from error import BasicError
+from services.cert import PrivateKey, Cert
+
+
+class ConfigServiceError(BasicError):
+    pass
+
+
+class ConfigService:
+    @staticmethod
+    def build_server_config(base_config_path: str, additional_lines: List[str] = None):
+        if not base_config_path:
+            raise ConfigServiceError('base config path is required')
+        if not os.path.exists(base_config_path):
+            raise ConfigServiceError('base config does not exist')
+
+        with open(base_config_path) as f:
+            base_config = f.read()
+
+        full_config = base_config
+        if additional_lines:
+            full_config = '%s\n%s' % (full_config, '\n'.join(additional_lines))
+        return full_config
+
+    @staticmethod
+    def build_client_config(base_config_path: str, ca_cert: Cert, client_cert: Cert, client_pkey: PrivateKey,
+                            tls_auth_key_path: str, additional_lines: List[str] = None):
+        if not base_config_path:
+            raise ConfigServiceError('base config path is required')
+        if not os.path.exists(base_config_path):
+            raise ConfigServiceError('base config does not exist')
+        if ca_cert is None:
+            raise ConfigServiceError('CA cert is required')
+        if client_cert is None:
+            raise ConfigServiceError('client cert is required')
+        if client_pkey is None:
+            raise ConfigServiceError('client pkey is required')
+        if not tls_auth_key_path:
+            raise ConfigServiceError('tls auth key path is required')
+        if not os.path.exists(tls_auth_key_path):
+            raise ConfigServiceError('tls auth key does not exist')
+
+        with open(base_config_path) as f:
+            base_config = f.read()
+
+        with open(tls_auth_key_path) as f:
+            tls_auth_key = f.read()
+
+        full_config = base_config
+        if additional_lines:
+            full_config = '%s\n%s' % (full_config, '\n'.join(additional_lines))
+        full_config = '%s\n<ca>\n%s</ca>\n<cert>\n%s</cert>\n<key>\n%s</key>\n<tls-auth>\n%s</tls-auth>\n' % (
+            full_config,
+            ca_cert.dump(),
+            client_cert.dump(),
+            client_pkey.dump(),
+            tls_auth_key
+        )
+        return full_config
---
+++ tests/test_config_service.py
@@ -0,0 +1,34 @@
+import os
+import unittest
+
+from services.cert import CertService
+from services.config import ConfigService
+
+data_folder = '/home/kelvin/openvpn-certs'
+
+
+class TestConfigService(unittest.TestCase):
+    def test_build_server_config(self):
+        cfg = ConfigService.build_server_config(os.path.join(data_folder, 'server.conf'), [
+            '# Test adding more lines',
+            '# Test adding more lines',
+            '# Test adding more lines'
+        ])
+        print(cfg)
+
+    def test_build_client_config(self):
+        ca_cert = CertService.load_cert(os.path.join(data_folder, 'openvpn-ca/keys/ca.crt'))
+
+        cert = CertService.load_cert(os.path.join(data_folder, 'openvpn-ca/keys/ymiao.crt'))
+        pkey = CertService.load_pkey(os.path.join(data_folder, 'openvpn-ca/keys/ymiao.key'))
+        tls_path = os.path.join(data_folder, 'openvpn-ca/keys/ta.key')
+
+        cfg = ConfigService.build_client_config(
+            os.path.join(data_folder, 'client-configs/base.conf'),
+            ca_cert, cert, pkey, tls_path,
+            [
+                '# Test adding more lines',
+                '# Test adding more lines',
+                '# Test adding more lines'
+            ])
+        print(cfg)
