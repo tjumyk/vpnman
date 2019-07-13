902269d5c047244c3116383caace7d09f4318117
tjumyk <tjumyk@gmail.com>
2019-07-13 16:42:07 +1000

    fix missing '__enter__' function for usage with 'with' statement

--- tools/manage.py
+++ tools/manage.py
@@ -28,6 +28,9 @@ class ManagementSession:
         # verify welcome message
         self._verify_welcome()
 
+    def __enter__(self):
+        return self
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.exit()
 
