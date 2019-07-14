ce4ef1c4976a733dad97e91af896541bc0691d47
tjumyk <tjumyk@gmail.com>
2019-07-15 00:40:40 +1000

    fix and improve logic and markup of error component

--- angular/src/app/error/error.component.ts
+++ angular/src/app/error/error.component.ts
@@ -1,21 +1,35 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit} from '@angular/core';
 import {BasicError} from "../models";
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.less']
+  styleUrls: ['./error.component.less'],
+  host: {class: 'ui error message'}
 })
 export class ErrorComponent implements OnInit {
-  @Input()
-  error: BasicError;
+  private _error: BasicError;
+  @Input() get error() {
+    return this._error;
+  }
 
-  is_cleared: boolean;
+  set error(error: BasicError) {
+    this._error = error;
+    this.updateHost();
+  }
 
-  constructor() {
+  constructor(private hostElement: ElementRef) {
   }
 
   ngOnInit() {
+  }
+
+  private updateHost(){
+    if (this._error) {
+      this.hostElement.nativeElement.classList.remove('hidden');
+    } else {
+      this.hostElement.nativeElement.classList.add('hidden');
+    }
   }
 
   redirect(url: string) {
