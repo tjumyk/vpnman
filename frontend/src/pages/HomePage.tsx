091a2d689426106edf64cf450b3a66a923e71b49
tjumyk <tjumyk@gmail.com>
2026-05-07 18:15:35 +0800

    refactor: restructure HomePage layout for improved responsiveness and visual clarity

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Alert, Anchor, Button, Card, Center, Grid, Group, Loader, SimpleGrid, Stack, Text } from '@mantine/core'
+import { Alert, Anchor, Button, Card, Center, Group, Loader, SimpleGrid, Stack, Text } from '@mantine/core'
 import { IconCircleCheck, IconCircleX, IconDownload, IconSettings, IconUser } from '@tabler/icons-react'
 import { useMemo, useState } from 'react'
 import { Link } from 'react-router-dom'
@@ -45,79 +45,79 @@ export function HomePage(): React.ReactElement {
         </Center>
       ) : null}
 
-      {client && activeCredential ? (
-        <Center style={isMobile ? undefined : { minHeight: '50vh' }}>
-          <SimpleGrid cols={{ base: 1, md: 2, xl: 4 }} w="100%" maw={1200}>
-            <Card withBorder>
-              <Text fw={700}>{t('homeLinuxConfigTitle')}</Text>
-              <Text size="sm" c="dimmed" mb="md">
-                {t('homeLinuxConfigDesc')}
-              </Text>
-              <Button
-                leftSection={<IconDownload size={16} />}
-                onClick={() => {
-                  setDownloadLinux(true)
-                  setShowDownloadModal(true)
-                }}
-              >
-                {t('commonDownload')}
-              </Button>
-            </Card>
-            <Card withBorder>
-              <Text fw={700}>{t('homeOtherConfigTitle')}</Text>
-              <Text size="sm" c="dimmed" mb="md">
-                {t('homeOtherConfigDesc')}
-              </Text>
-              <Button
-                leftSection={<IconDownload size={16} />}
-                onClick={() => {
-                  setDownloadLinux(false)
-                  setShowDownloadModal(true)
-                }}
-              >
-                {t('commonDownload')}
-              </Button>
-            </Card>
-            <Card withBorder>
-              <Text fw={700}>{t('homeSetupGuideTitle')}</Text>
-              <Text size="sm" c="dimmed" mb="md">
-                {t('homeSetupGuideDesc')}
-              </Text>
-              <Button component={Link} to="/client-setup" variant="light">
-                {t('homeSetupGuideAction')}
-              </Button>
-            </Card>
-            <Card withBorder>
-              <Text fw={700}>{t('homeMyClientTitle')}</Text>
-              <Text size="sm" c="dimmed" mb="md">
-                {t('homeMyClientDesc')}
-              </Text>
-              <Button component={Link} to="/my-client" variant="light">
-                {t('homeMyClientAction')}
-              </Button>
-            </Card>
-          </SimpleGrid>
-        </Center>
-      ) : client ? (
-        <Alert color="yellow" title={t('homeNoCredentialsTitle')}>
-          {t('homeNoCredentialsBody')}
-        </Alert>
-      ) : null}
+      <Center>
+        <Stack w="100%" maw={1200}>
+          {client && activeCredential ? (
+            <Center style={isMobile ? undefined : { minHeight: '50vh' }}>
+              <SimpleGrid cols={{ base: 1, md: 2, xl: 4 }} w="100%">
+                <Card withBorder>
+                  <Text fw={700}>{t('homeLinuxConfigTitle')}</Text>
+                  <Text size="sm" c="dimmed" mb="md">
+                    {t('homeLinuxConfigDesc')}
+                  </Text>
+                  <Button
+                    leftSection={<IconDownload size={16} />}
+                    onClick={() => {
+                      setDownloadLinux(true)
+                      setShowDownloadModal(true)
+                    }}
+                  >
+                    {t('commonDownload')}
+                  </Button>
+                </Card>
+                <Card withBorder>
+                  <Text fw={700}>{t('homeOtherConfigTitle')}</Text>
+                  <Text size="sm" c="dimmed" mb="md">
+                    {t('homeOtherConfigDesc')}
+                  </Text>
+                  <Button
+                    leftSection={<IconDownload size={16} />}
+                    onClick={() => {
+                      setDownloadLinux(false)
+                      setShowDownloadModal(true)
+                    }}
+                  >
+                    {t('commonDownload')}
+                  </Button>
+                </Card>
+                <Card withBorder>
+                  <Text fw={700}>{t('homeSetupGuideTitle')}</Text>
+                  <Text size="sm" c="dimmed" mb="md">
+                    {t('homeSetupGuideDesc')}
+                  </Text>
+                  <Button component={Link} to="/client-setup" variant="light">
+                    {t('homeSetupGuideAction')}
+                  </Button>
+                </Card>
+                <Card withBorder>
+                  <Text fw={700}>{t('homeMyClientTitle')}</Text>
+                  <Text size="sm" c="dimmed" mb="md">
+                    {t('homeMyClientDesc')}
+                  </Text>
+                  <Button component={Link} to="/my-client" variant="light">
+                    {t('homeMyClientAction')}
+                  </Button>
+                </Card>
+              </SimpleGrid>
+            </Center>
+          ) : client ? (
+            <Alert color="yellow" title={t('homeNoCredentialsTitle')}>
+              {t('homeNoCredentialsBody')}
+            </Alert>
+          ) : null}
 
-      {loadingServerStatus ? (
-        <Text c="dimmed">{t('homeCheckingServerStatus')}</Text>
-      ) : serverStatus ? (
-        <Alert
-          color={serverStatus.online ? 'green' : 'red'}
-          icon={serverStatus.online ? <IconCircleCheck size={16} /> : <IconCircleX size={16} />}
-        >
-          {serverStatus.online ? t('homeServerOnline') : t('homeServerOffline')}
-        </Alert>
-      ) : null}
+          {loadingServerStatus ? (
+            <Text c="dimmed">{t('homeCheckingServerStatus')}</Text>
+          ) : serverStatus ? (
+            <Alert
+              color={serverStatus.online ? 'green' : 'red'}
+              icon={serverStatus.online ? <IconCircleCheck size={16} /> : <IconCircleX size={16} />}
+            >
+              {serverStatus.online ? t('homeServerOnline') : t('homeServerOffline')}
+            </Alert>
+          ) : null}
 
-      {user ? (
-        <Grid>
-          <Grid.Col span={12}>
+          {user ? (
             <Group>
               <Button component="a" href="/account/profile" variant="default" leftSection={<IconUser size={16} />}>
                 {user.nickname || user.name}
@@ -128,13 +128,13 @@ export function HomePage(): React.ReactElement {
                 </Button>
               ) : null}
             </Group>
-          </Grid.Col>
-        </Grid>
-      ) : null}
+          ) : null}
 
-      <Anchor component={Link} to="/my-client">
-        {t('homeGoMyClient')}
-      </Anchor>
+          <Anchor component={Link} to="/my-client">
+            {t('homeGoMyClient')}
+          </Anchor>
+        </Stack>
+      </Center>
     </Stack>
   )
 }
